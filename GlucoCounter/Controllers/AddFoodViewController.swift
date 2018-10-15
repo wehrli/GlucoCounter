69cbd2b39afa0c215a84f0a6556bdfb486c257cb
Moi <om@macbook-pro-de-guillaume.local>
2018-10-15 20:42:02 +0200

    la mise a jour de swift n'a pas étai faite ! MAIS le bug était du a la conversion d'une string vers float => Solution (valeur as NSString).floatValue
    Autre bug : problem d'envoi du model core data (sur le telephone) => Solution refaire complètement le model (c'était la seule solution apparement)

--- GlucoCounter/Controllers/AddFoodViewController.swift
+++ GlucoCounter/Controllers/AddFoodViewController.swift
@@ -52,29 +52,50 @@ class AddFoodViewController: UIViewController {
             return
         }
         
-        let numberFormatter = NumberFormatter()
+        let numberFormatter: NumberFormatter! = NumberFormatter()
         
         // Weight string to Float
         let quantityChooseByUser = numberFormatter.number(from: quantity.text!)?.floatValue
         
         // Get food composant string to format into Float, from RawFood
-        let carbohydrateForCentGrams = numberFormatter.number(from: value[0].carbohydrate!)?.floatValue
-        let fiberForCentGrams = numberFormatter.number(from: value[0].fibres!)?.floatValue
-        let kcalForCentGrams = numberFormatter.number(from: value[0].kcal!)?.floatValue
+        var carbohydrate: Float! = 0
+        var fibre: Float! = 0
+        var kcal: Float! = 0
+        
+        var tmp = value[0].carbohydrate!.description
+        carbohydrate = (tmp as NSString).floatValue
+        tmp = value[0].fibres!.description
+        fibre = (tmp as NSString).floatValue
+        tmp = value[0].kcal!.description
+        kcal = (tmp as NSString).floatValue
+        
+//        if let carboValueOptionalValue = value[0].carbohydrate?.description {
+//            carbohydrate = numberFormatter.number(from:carboValueOptionalValue)?.floatValue ?? 0
+//        }
+//        if let fiberValueOptionalValue = value[0].fibres {
+//            fibre = numberFormatter.number(from:fiberValueOptionalValue)?.floatValue ?? 0
+//        }
+//        if let kcalValueOptionalValue = value[0].kcal {
+//            kcal = numberFormatter.number(from:kcalValueOptionalValue)?.floatValue ?? 0
+//        }
+        
+//        let carbohydrateForCentGrams = numberFormatter.number(from: value[0].carbohydrate!)!.floatValue
+//        let fiberForCentGrams = numberFormatter.number(from: value[0].fibres!)?.floatValue
+//        let kcalForCentGrams = numberFormatter.number(from: value[0].kcal!)?.floatValue
         
         //calcul glucidique (
-        let carbohydrate = (quantityChooseByUser! * carbohydrateForCentGrams!) / 100
-        let fiber = (quantityChooseByUser! * fiberForCentGrams!) / 100
-        let glucides = carbohydrate - fiber
+        let carbohydrateQ = (quantityChooseByUser! * carbohydrate) / 100
+        let fiberQ = (quantityChooseByUser! * fibre) / 100
+        let glucides = carbohydrateQ - fiberQ
         
         //calcul kCal
-        let kCal = (quantityChooseByUser! * kcalForCentGrams!) / 100
+        let kCalQ = (quantityChooseByUser! * kcal) / 100
         
         
         let itemToAdd = FoodItemMO(context: DBManager.sharedInstance.getContext())
         itemToAdd.name = titleInput.text
         itemToAdd.glucidicQuantity = glucides
-        itemToAdd.kCalQuantity = kCal
+        itemToAdd.kCalQuantity = kCalQ
         itemToAdd.weight = quantityChooseByUser!
         
             
